98347bd87cf3b386c2766faf7ecd7c03ce488a70
HermitOwl <jasex.2000@gmail.com>
2021-03-18 12:18:30 +0000

    Test 2 Adding contructor to Node Classs

--- Step1/src/com/coursework2/dynamicdatatypes/Node.java
+++ Step1/src/com/coursework2/dynamicdatatypes/Node.java
@@ -1,5 +1,14 @@
 package com.coursework2.dynamicdatatypes;
 
 public class Node {
+	private Object payload;
+	private Node branchA;
+	private Node branchB;
+	
+	
+	public Node(Object payload)
+	{
+		this.payload = payload;
+	}
 
 }
