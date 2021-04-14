2d154c44c883fabfa49547e2f462c91e06a92d63
HermitOwl <jasex.2000@gmail.com>
2021-04-14 12:36:17 +0100

    Minor Additions

--- Step1/src/com/coursework2/dynamicdatatypes/BinaryTree.java
+++ Step1/src/com/coursework2/dynamicdatatypes/BinaryTree.java
@@ -2,12 +2,15 @@ package com.coursework2.dynamicdatatypes;
 
 public class BinaryTree {
 	private Node rootNode;
+	private Node pointerNode;
 	
 	
 	public BinaryTree(Node rootNode) 
 	{
 		this.rootNode = rootNode ;
+		pointerNode = rootNode;
 	}
+	
 	
 	
 	public Node getRootNode() 
@@ -15,8 +18,39 @@ public class BinaryTree {
 		return rootNode;
 	}
 	
-	public void compareNodes() 
+	public Node getPointerNode() 
+	{
+		return pointerNode;
+	}
+	
+	public void setPointerNode(Node pointerNode)
 	{
 		
+		this.pointerNode = pointerNode;
 	}
+	
+	
+	
+	public void compareNodes(Node a, Node b) 
+	{
+		//TODO complete this!!
+	}
+	
+	public void traverseTree() 
+	{
+		//TODO complete this!!
+	}
+	
+	public void insertNode() 
+	{
+		//TODO Complete this!
+	}
+	
+	public void RemoveNode() 
+	{
+		//TODO Complete this!
+	}
+	
+	
+	
 }
